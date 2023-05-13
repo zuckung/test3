f631a9c82b8b5031d83095024d2118a0f70a10a6
zuckung <zuckung@gmx.de>
2023-05-13 07:57:10 +0200

    gggg

--- res/src/makemd.py
+++ res/src/makemd.py
@@ -60,7 +60,7 @@ for entry in entries:
 		status = status.split("=")[1]
 		status = status.replace("\n", "")
 		description = file2.readlines()
-		description.remove("description = \n")
+		description.remove("description=")
 	file2.close 	
 	
 	# get last modified date from the assetfiles
